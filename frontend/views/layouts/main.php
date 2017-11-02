65f5a505f657714db0df049c1e214d17390ce5b8
Ivan A <ivanhub@webera.pro>
2017-11-02 10:22:04 +0400

    11.2.4

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -49,10 +49,15 @@ $this->beginPage() ?>
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Security-Policy" content="font-src 'self' *.ros.academy fonts.gstatic.com fonts.googleapis.com; style-src 'self' 'unsafe-inline' fonts.googleapis.com maxcdn.bootstrapcdn.com; script-src 'self' 'unsafe-inline' 'unsafe-eval' www.googleadservices.com www.google-analytics.com mc.yandex.ru *.ros.academy www.google-analytics.com apis.google.com connect.facebook.net connect.ok.ru ajax.googleapis.com">
     <meta name="format-detection" content="telephone=no">
     <meta http-equiv="cleartype" content="on"/>
+    <!-- IE -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-    <meta http-equiv="msthemecompatible" content="no"/>
+    <!--[if IE]>
+    <meta http-equiv="imagetoolbar" content="no"/>
+    <meta http-equiv="MSThemeCompatible" content="no"/>
+    <![endif]-->
     <meta name="author" content="Ivan Alexandrov / Ros.Academy"/>
     <meta name="copyright" content="Ros.Academy (c)"> 
     <?= Html::csrfMetaTags() ?>
