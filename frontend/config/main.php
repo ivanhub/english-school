36bbcb632373ecbd9d02a0a0d686f48a9cca37d2
Ivan A <ivanhub@webera.pro>
2017-12-18 13:31:17 +0400

    composer update & drivers page update & add js/css optimization

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -13,7 +13,34 @@ return [
     'bootstrap' => ['mobileDetect'],
     'controllerNamespace' => '\frontend\controllers',
     
+  
     'components' => [
+      'view' => [
+            'class' => '\processfast\yii\minify\View',
+            //'enableMinify' => !YII_DEBUG,
+            'enableMinify' => true,
+            'concatCss' => true, // concatenate css
+            'minifyCss' => true, // minificate css
+            'concatJs' => true, // concatenate js
+            'minifyJs' => true, // minificate js
+            'minifyOutput' => true, // minificate result html page
+            'webPath' => '@web', // path alias to web base
+            'basePath' => '@webroot', // path alias to web base
+            'minifyPath' => '@webroot/assets/minified', // path alias to save minify result
+            'jsPosition' => [ \yii\web\View::POS_END ], // positions of js files to be minified
+            'forceCharset' => 'UTF-8', // charset forcibly assign, otherwise will use all of the files found charset
+            'expandImports' => true, // whether to change @import on content
+            'compressOptions' => ['extra' => true, 'no-comments' => true], // options for compress
+            'excludeFiles' => [
+                'jquery.js', // exclude this file from minification
+                'app-[^.].js', // you may use regexp
+                'allanidm.css', // you may use regexp
+    ],
+    'excludeBundles' => [
+            //    \dev\helloworld\AssetBundle::class, // exclude this bundle from minification
+    ],
+],
+
 
 
 
